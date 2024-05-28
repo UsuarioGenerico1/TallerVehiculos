c11804ef41e91e6f3a5b79f2b9e0b530cdd8714a
Neloj <nemuisen@proton.me>
2024-05-27 21:53:02 -0500

    me falto una funcion en la clase Mantenimiento

--- TallerVehiculos/Datos/Mantenimiento.cs
+++ TallerVehiculos/Datos/Mantenimiento.cs
@@ -8,14 +8,38 @@ namespace TallerVehiculos.Datos
 {
     internal class Mantenimiento
     {
-        int Codigo_Mantenimiento;
-        int ID_Cliente;
-        int ID_Mecanico;
-        DateTime Fecha_Mantenimiento;
-        int ID_Vehiculo;
-        String Diagnostico;
-        String Trabajos_Realizados;
-        String Tipo_Mantenimiento;
-        int ID_Servicios;
+        int codigo_mantenimiento;
+        int id_cliente;
+        int id_mecanico;
+        DateTime fecha_mantenimiento;
+        int id_vehiculo;
+        string diagnostico;
+        string trabajos_realizados;
+        string tipo_mantenimiento;
+        int id_servicios;
+
+        public Mantenimiento(
+            int codigo_mantenimiento,
+            int id_cliente,
+            int id_mecanico,
+            datetime fecha_mantenimiento,
+            int id_vehiculo,
+            string diagnostico,
+            string trabajos_realizados,
+            string tipo_mantenimiento,
+            int id_servicios
+        ) {
+            this.codigo_mantenimiento = codigo_mantenimiento;
+            this.id_cliente = id_cliente;
+            this.id_mecanico = id_mecanico;
+            this.fecha_mantenimiento = fecha_mantenimiento;
+            this.id_vehiculo = id_vehiculo;
+            this.diagnostico = diagnostico;
+            this.trabajos_realizados = trabajos_realizados;
+            this.tipo_mantenimiento = tipo_mantenimiento;
+            this.id_servicios = id_servicios;
+        }
+
+        void CalcularPrecioMantenimiento() {}
     }
 }
